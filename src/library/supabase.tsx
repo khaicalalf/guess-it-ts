5dbf5e8f1cae716ecd5b44d4477fd9c4c4bf8337
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2025-10-15 09:50:23 +0700

    update supabase
    pindah project supabase

--- src/library/supabase.tsx
+++ src/library/supabase.tsx
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ngphpqmwoglidgfertqv.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ncGhwcW13b2dsaWRnZmVydHF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1OTgzNTgsImV4cCI6MjA2MDE3NDM1OH0.yg8m76qPoPFdDTHKfNofzEzmxehTTSNKBlQ_QWpGcz4";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
